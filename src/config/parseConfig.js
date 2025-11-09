e24bc54e26020695e0c9a817c73d3c6cb9c0e8fe
hajar <hajarboukhlik123@gmail.com>
2025-11-09 22:43:10 +0100

    Añadido crypto y bolsa (Markel) y cambio Contraseña (Artem)

--- src/config/parseConfig.js
+++ src/config/parseConfig.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
 const Parse = require("parse/node");
 
+// Configuración de Parse
 Parse.initialize(
-  process.env.APP_ID,
+  process.env.APP_ID,     // Usar variables de entorno
   process.env.CLIENT_KEY,
   process.env.MASTER_KEY
 );
 Parse.serverURL = "https://parseapi.back4app.com/";
 
-console.log(" MASTER_KEY:", process.env.MASTER_KEY ? "Cargada correctamente" : "NO cargada");
+console.log("MASTER_KEY:", process.env.MASTER_KEY);
+
 
 module.exports = Parse;
